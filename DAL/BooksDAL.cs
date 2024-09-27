f69732ef09af613dc12b33840acd7479ae9bf178
Ahmad Kassem <75832271+Ahmad82450@users.noreply.github.com>
2024-09-27 09:04:29 +0200

    Installed the required packages, Add the DAL and BLL classs libraries Created a MS test project and added to the

---
+++ DAL/BooksDAL.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class BooksDAL
+    {
+        //MySql.Data.MySqlClient.MySqlConnection myConnection;
+        //private string connectionString = "";
+
+        //public void GetAllBooks()
+        //{
+        //    try
+        //    {
+        //        connectionString = "server=127.0.0.1;uid=root;pwd=12345;database=test";
+
+        //        myConnection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+
+        //        myConnection.Open();
+
+        //        MySqlCommand myCommand = new MySqlCommand();
+        //        myCommand.Connection = myConnection;
+        //        myCommand.CommandText = @"SELECT * FROM Books";
+
+        //        using var myReader = myCommand.ExecuteReader();
+
+        //        while (myReader.Read())
+        //        {
+
+        //        }
+        //    };
+
+        //}
+
+    }
+}
